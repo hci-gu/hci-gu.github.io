97f848e704be37a68e7b84145bf8780465f54a00
sebastianandreasson <sebastian.andreasson93@gmail.com>
2020-08-26 08:27:21 +0000

    Deploying to gh-pages from  @ 13f14703460279b34860232e2308b72f230cd079 🚀

---
+++ precache-manifest.2288fdf928bd34a3af49cdcc6b7a1931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fa3cb1ccd905ae235b9df30f204ac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8052cbc6fe08875ef140",
+    "url": "/static/js/2.7dfee6dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7dfee6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02507855d8dabc141911",
+    "url": "/static/js/main.00dd9c4c.chunk.js"
+  },
+  {
+    "revision": "158543f78ff3ff884e7d",
+    "url": "/static/js/runtime-main.bfe30713.js"
+  }
+]);
