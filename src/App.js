7e91c170c7a4a7a17ba0426e7e869f6fad9c3a3d
Sebastian Andreasson <xandse@ml-200403-001.local>
2020-09-23 10:12:24 +0200

    add temp email message

--- src/App.js
+++ src/App.js
@@ -5,8 +5,38 @@ const Container = styled.div`
   /* padding-bottom: 100px; */
 `
 
+const TempEmail = styled.div`
+  position: fixed;
+  background-color: #f5853f;
+  padding: 40px;
+  bottom: 40px;
+  left: 40px;
+
+  font-size: 20px;
+  border-radius: 10px;
+
+  > a {
+    color: black;
+    font-weight: bold;
+  }
+
+  z-index: 99999;
+`
+
 const App = ({ children }) => {
-  return <Container>{children}</Container>
+  return (
+    <Container>
+      {children}
+      <TempEmail>
+        The email at Gothenburg University is currently down.<br></br> Please
+        reach out to{' '}
+        <a href="mailto:me@sebastianandreasson.com">
+          me@sebastianandreasson.com
+        </a>{' '}
+        if you need to reach us.
+      </TempEmail>
+    </Container>
+  )
 }
 
 export default App
