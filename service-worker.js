f3599d02813374eb8457dd3ea0009f6d88ce91ed
sebastianandreasson <sebastian.andreasson93@gmail.com>
2020-10-22 12:46:12 +0000

    Deploying to gh-pages from  @ 0952add7b3c6402b24a27617d92395c5efe790e3 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b148d905d90e8afffe3399a819661b76.js"
+  "/precache-manifest.49e9f3ffec13a67068654f966eca8683.js"
 );
 
 self.addEventListener('message', (event) => {
