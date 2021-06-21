b4111d5006afb5a8446629a871faadd3c113e813
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2021-06-21 10:56:20 +0200

    replace CRA with vite

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+// import * as path from 'path';
+import { UserConfig } from 'vite'
+// import vitePluginImp from 'vite-plugin-imp';
+
+// https://vitejs.dev/config/
+export default {
+  plugins: [],
+  server: {
+    // open: true,
+  },
+  resolve: {
+    alias: [
+      // { find: '@', replacement: path.resolve(__dirname, 'src') },
+      // fix less import by: @import ~
+      // https://github.com/vitejs/vite/issues/2185#issuecomment-784637827
+      { find: /^~/, replacement: '' },
+    ],
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+    preprocessorOptions: {
+      less: {
+        // modifyVars: { 'primary-color': '#13c2c2' },
+        javascriptEnabled: true,
+      },
+    },
+  },
+}
