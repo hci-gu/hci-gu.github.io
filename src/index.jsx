06dea44e5cc41ff9ab8c2142c5166028cadbb3fa
Sebastian Andreasson <xandse@MacBook-Pro-som-tillhor-Sebastian.local>
2021-09-29 17:29:16 +0200

    cookie-consent popup

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { RecoilRoot } from 'recoil'
 import { createClient, Provider } from 'urql'
 import 'antd/dist/antd.less'
 import App from './App'
+import CookieConsent from 'react-cookie-consent'
 
 const client = createClient({
   url: 'https://graphql.contentful.com/content/v1/spaces/j07xal62e1un',
@@ -22,6 +23,31 @@ const Root = () => {
       <Provider value={client}>
         <RecoilRoot>
           <App />
+          <CookieConsent
+            location="bottom"
+            buttonText="Jag förstår"
+            cookieName="cookie-consent"
+            style={{
+              background: '#FBFBFB',
+              color: 'black',
+              border: '1px solid rgba(0,0,0,0.25)',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+            buttonStyle={{
+              background: '#1b4079',
+              borderRadius: '4px',
+              color: 'white',
+              fontSize: '13px',
+            }}
+            contentStyle={{
+              flex: '1 1 1',
+            }}
+            expires={150}
+          >
+            Den här hemsidan använder cookies för att förbättra din
+            användarupplevelse.
+          </CookieConsent>
         </RecoilRoot>
       </Provider>
     </React.StrictMode>
