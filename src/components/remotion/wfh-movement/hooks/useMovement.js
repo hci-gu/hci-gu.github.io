310d0b8f3a9a5f808405296b0a873aa6c3d556de
Sebastian Andreasson <xandse@MacBook-Pro-som-tillhor-Sebastian.local>
2021-10-28 14:04:33 +0200

    tweak hook layout

--- src/components/remotion/wfh-movement/hooks/useMovement.js
+++ src/components/remotion/wfh-movement/hooks/useMovement.js
@@ -50,35 +50,35 @@ const useMovement = () => {
   const rotateX2 = spring({
     fps,
     from: 0,
-    to: width < 940 ? 0 : -Math.PI / 5,
+    to: width < 720 ? 0 : -Math.PI / 5,
     frame: secondaryMotion,
     config: baseSpringConfig,
   })
   const left = spring({
     fps,
     from: 0,
-    to: width < 940 ? 0 : 3,
+    to: width < 720 ? 0 : 3,
     frame: secondaryMotion,
     config: baseSpringConfig,
   })
   const scaleX = spring({
     fps,
     from: -1.05,
-    to: width < 940 ? -1.05 : -0.9,
+    to: width < 720 ? 1.05 : -0.9,
     frame: secondaryMotion,
     config: baseSpringConfig,
   })
   const scale = spring({
     fps,
     from: 1,
-    to: width < 940 ? 0.6 : 1,
+    to: width < 720 ? 0.6 : 1,
     frame: secondaryMotion,
     config: baseSpringConfig,
   })
   const top2 = spring({
     fps,
     from: 0,
-    to: width < 940 ? -1.5 : 0,
+    to: width < 720 ? -1.5 : 0,
     frame: secondaryMotion,
     config: baseSpringConfig,
   })
